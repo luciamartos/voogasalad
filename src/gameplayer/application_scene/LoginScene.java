ce8c8017f88718b5645892a8e96287743a2107c7
TeddyMarchildon <tmm61@duke.edu>
2016-11-15 16:51:34 -0500

    Added initial scene files and created basic flow of app

---
+++ src/gameplayer/application_scene/LoginScene.java
@@ -0,0 +1,43 @@
+package gameplayer.application_scene;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * Login Scene Initiation
+ *
+ * @author tedmarchildon, hannah
+ *
+ */
+public class LoginScene extends AbstractPlayerScene {
+
+	public LoginScene(Stage astage) {
+		super(astage);
+	}
+
+	@Override
+	public Scene init() {
+		myRoot.setCenter(addNodes());
+		return myScene;
+	}
+	
+	private VBox addNodes(){
+		VBox options = new VBox(BOX_INSETS);
+		options.getChildren().add(createTextField("Enter Username", 0, 0, 500));
+		options.getChildren().add(createTextField("Enter Password", 0, 0, 500));
+		HBox loginOptions = new HBox(BOX_INSETS);
+		loginOptions.getChildren().add(createButton("Enter", 0, 0, e -> {
+			transitionScene(new MainMenuScene(myStage));
+		}));
+		loginOptions.getChildren().add(createButton("Sign Up", 0, 0, e -> {
+			//TODO: Implement Signing Up
+		}));
+		loginOptions.setAlignment(Pos.CENTER);
+		options.getChildren().add(loginOptions);
+		options.setAlignment(Pos.CENTER);
+		return options;
+	}
+}
