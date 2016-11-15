ca3aaab76221d66f3f54ecbbc7126bc77a206cf8
Lucia Martos <lm260@duke.edu>
2016-11-14 17:33:56 -0800

    change

--- src/game_engine/PlayerSpriteStatus.java
+++ src/game_engine/PlayerSpriteStatus.java
@@ -1,9 +1,42 @@
 package game_engine;
 
-public class PlayerSpriteStatus {
+import javafx.scene.image.Image;
 
-	public PlayerSpriteStatus() {
-		// TODO Auto-generated constructor stub
+public class PlayerSpriteStatus {
+	
+	private double xCordinate;
+	private double yCordinate;
+	private Image playerImage;
+
+	public PlayerSpriteStatus(double xCordinate, double yCordinate, Image playerImage) {
+		this.xCordinate = xCordinate;
+		this.yCordinate = yCordinate;
+		this.playerImage = playerImage;
 	}
+	
+	public double getxCordinate() {
+		return this.xCordinate;
+	}
+
+	public void setxCordinate(double xCordinate) {
+		this.xCordinate = xCordinate;
+	}
+
+	public double getyCordinate() {
+		return this.yCordinate;
+	}
+
+	public void setyCordinate(double yCordinate) {
+		this.yCordinate = yCordinate;
+	}
+
+	public Image getPlayerImage() {
+		return this.playerImage;
+	}
+
+	public void setPlayerImage(Image playerImage) {
+		this.playerImage = playerImage;
+	}
+	
 
 }
