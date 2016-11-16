f0164b32462095dd09d1b041d3d5e88357c368c3
TeddyMarchildon <tmm61@duke.edu>
2016-11-16 13:24:03 -0500

    removed strings from the scene factory

--- src/gameplayer/application_scene/SceneFactory.java
+++ src/gameplayer/application_scene/SceneFactory.java
@@ -1,17 +1,15 @@
 package gameplayer.application_scene;
 
-import javafx.stage.Stage;
-
 public class SceneFactory {
 
 	public IDisplay create(String aIdentifier){
-		if(aIdentifier.equals("LOGIN")){
+		if(aIdentifier.equals(SceneIdentifier.LOGIN.toString())){
 			return new LoginScene();
-		} else if(aIdentifier.equals("MAINMENU")){
+		} else if(aIdentifier.equals(SceneIdentifier.MAINMENU.toString())){
 			return new MainMenuScene();
-		} else if(aIdentifier.equals("GAMECHOICE")){
+		} else if(aIdentifier.equals(SceneIdentifier.GAMECHOICE.toString())){
 			return new GameChoiceScene();
-		} else if(aIdentifier.equals("GAMEPLAY")){
+		} else if(aIdentifier.equals(SceneIdentifier.GAMEPLAY.toString())){
 			return new GamePlayScene();
 		} else {
 			return null;
