3e1ca64bdbdf914d6fdb15afd8db52a1d7803c63
Hannah Fuchshuber <hpf3@duke.edu>
2016-11-17 13:51:24 -0500

    fixed error

--- src/gameplayer/animation_loop/AnimationLoop.java
+++ src/gameplayer/animation_loop/AnimationLoop.java
@@ -5,29 +5,6 @@ import javafx.animation.Timeline;
 import javafx.util.Duration;
 
 public class AnimationLoop {
-	private static final int FRAMES_PER_SECOND = 1;
-    private static final int MILLISECOND_DELAY = 1000/FRAMES_PER_SECOND;
-    
-	private Timeline animation;
-	/**
-	 * Constructor that takes in metadata and the grid 
-	 * @param meta - the MetaData object
-	 * @param grid - the grid
-	 */
-	public AnimationLoop() {
-		this.animation = new Timeline();
-	}
-	
-	
-	public void init() {
-		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step());
-		animation.setCycleCount(Timeline.INDEFINITE);
-		animation.getKeyFrames().add(frame);
-		animation.play();
-	}
-
-	private void step() {
-	}
 
 	private static final int FRAMES_PER_SECOND = 1;
     private static final int MILLISECOND_DELAY = 1000/FRAMES_PER_SECOND;
