5e7d25c7f780b6b88299901598719e4af375005a
Cleveland Thompson <ct168@duke.edu>
2016-11-18 15:19:38 -0500

    Merging game data

--- src/game_data/GameDataTestingMain.java
+++ src/game_data/GameDataTestingMain.java
@@ -1,10 +1,13 @@
 package game_data;
 
+import author.model.game_observables.observable_level.ObservableLevel;
+import author.model.game_observables.observable_level.ObservableLevelFactory;
+
 public class GameDataTestingMain {
 
 	public static void main(String[] args){
 		Sprite aSprite = new Player(new Location(0,0,0), "path/spriteImage.jpg");
-		Level aLevel = new Level(500, 8000, "path/backgroundImage.jpg");
+		ObservableLevel aLevel = new ObservableLevelFactory().create(500, 500, "Image");
 		aLevel.addNewSprite(aSprite);
 		Game aGame = new Game();
 		aGame.addNewLevel(aLevel);
