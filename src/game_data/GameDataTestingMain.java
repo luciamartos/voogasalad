6cf8552149da65b440a60813d3c6a5cb16f0119a
Austin <aeg36@duke.edu>
2016-11-19 22:37:21 -0500

    engine touchups

--- src/game_data/GameDataTestingMain.java
+++ src/game_data/GameDataTestingMain.java
@@ -5,7 +5,7 @@ import game_data.sprites.Player;
 public class GameDataTestingMain {
 
 	public static void main(String[] args){
-		Sprite aSprite = new Player(new Location(0,0,0), "path/spriteImage.jpg", 10);
+		Sprite aSprite = new Player(new Location(0,0,0), "path/spriteImage.jpg", 10, 100, 100);
 		Level aLevel = new Level(500, 8000, "path/backgroundImage.jpg");
 		aLevel.addNewSprite(aSprite);
 		Game aGame = new Game();
