6b73cb08ad92ad69f7894e4a88e0dc242647dd24
Addison Howenstine <arh55@duke.edu>
2016-11-27 21:15:25 -0500

    Add save and load to authoring environment. Currently debugging because program is currently trying to serialize something from JavaFX though we don't know what could possibly include JavaFX

--- src/author/model/IAuthorModel.java
+++ src/author/model/IAuthorModel.java
@@ -3,11 +3,13 @@
  */
 package author.model;
 
+import java.io.File;
+
 import game_data.Game;
 import game_data.Sprite;
 
 /**
- * @author Cleveland Thompson V (ct168)
+ * @author Cleveland Thompson V (ct168), Addison Howenstine
  *
  */
 public interface IAuthorModel {
@@ -17,4 +19,8 @@ public interface IAuthorModel {
 	public void newGame();
 	
 	public Game getGame();
+	
+	public void saveGame(String aFileName);
+	
+	public void loadGame(File aFile);
 }
