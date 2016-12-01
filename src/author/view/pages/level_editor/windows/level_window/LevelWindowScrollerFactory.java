5085629e09f2290a879244b08b52f1234c5c38b6
Cleveland Thompson <ct168@duke.edu>
2016-12-01 01:17:13 -0500

    Working on getting background to appear
    Former-commit-id: 2fe6275e31f3716975f79c85d512f724bc94200c

--- src/author/view/pages/level_editor/windows/level_window/LevelWindowScrollerFactory.java
+++ src/author/view/pages/level_editor/windows/level_window/LevelWindowScrollerFactory.java
@@ -7,6 +7,7 @@ package author.view.pages.level_editor.windows.level_window;
 import author.view.pages.level_editor.windows.ILevelEditorWindowInternal;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.layout.Pane;
 
 /**
  * @author Cleveland Thompson V (ct168)
@@ -25,12 +26,11 @@ public class LevelWindowScrollerFactory {
 	
 	public ScrollPane create(){
 		this.levelScroller = new ScrollPane();
-		this.levelScroller.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
-		this.levelScroller.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		this.levelScroller.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+		this.levelScroller.setHbarPolicy(ScrollBarPolicy.ALWAYS);
 
-		this.levelScroller.prefViewportWidthProperty().bind(this.levelEditorWindowInternal.getWindow().widthProperty());
-		this.levelScroller.prefViewportHeightProperty().bind(this.levelEditorWindowInternal.getWindow().heightProperty());
-
+		//this.levelScroller.prefViewportWidthProperty().bind(this.levelEditorWindowInternal.getWindow().widthProperty());
+		//this.levelScroller.prefViewportHeightProperty().bind(this.levelEditorWindowInternal.getWindow().heightProperty());
 		return this.levelScroller;
 	}
 
