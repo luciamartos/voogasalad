8cf0c2840e3414161e1a5fa07c1fe84acd1b444d
Lucia Martos <lm260@duke.edu>
2016-12-08 20:21:20 -0500

    addded score based winning on position
    Former-commit-id: 049e2fe0bd72966e3ce653240f2c7bec1f2e956d

--- src/game_data/characteristics/ScoreBasedOnPosition.java
+++ src/game_data/characteristics/ScoreBasedOnPosition.java
@@ -13,68 +13,33 @@ import game_data.characteristics.characteristic_annotations.ParameterAnnotation;
 import game_data.sprites.Player;
 import game_engine.actions.Action;
 import game_engine.actions.Break;
+import game_engine.actions.ScoreAdder;
 import javafx.geometry.Side;
 
 @CharacteristicAnnotation(name = "Score Based On Position")
-public class ScoreBasedOnPosition implements Characteristic{
+public class ScoreBasedOnPosition implements Characteristic {
 
-	private boolean breakableNorth;
-	private boolean breakableSouth;
-	private boolean breakableEast;
-	private boolean breakableWest;
-	private int myDurability;
-	private Action myAction;
 	private Sprite mySprite;
-	
-	@ParameterAnnotation(parameters = {"North Side", "South Side", "East Side", "West Side", "Durability", "Sprite"})
-	public ScoreBasedOnPosition(boolean north, boolean south, boolean east, boolean west, int durability, Sprite aSprite){
-		breakableNorth = north;
-		breakableSouth = south;
-		breakableEast = east;
-		breakableWest = west;
-		myDurability = durability;
+	private double initX;
+	private double initY;
+
+	@ParameterAnnotation(parameters = { "Sprite" })
+	public ScoreBasedOnPosition(Sprite aSprite, double initX, double initY) {
 		mySprite = aSprite;
+		this.initX = initX;
+		this.initY = initY;
 	}
-	
-	public void setDurability(int durability){
-		myDurability = durability;
-	}
-	
+
 	@Override
-	public void execute(Map<Sprite, Side> myCollisionMap){
-		for(Sprite collidedSprite:myCollisionMap.keySet()){
-			if(breaksAtDirection(myCollisionMap.get(collidedSprite)) && collidedSprite instanceof Player){
-//				System.out.println("SIDE HIT: "+myCollisionMap.get(collidedSprite));
-				if(isBroken()) {
-					myAction = new Break(mySprite, collidedSprite);
-					myAction.act();
-				}
-			}
-		}
-	}
-	
-	private boolean isBroken() {
-		myDurability--;
-		return myDurability<=0;
-	}
-	
-	private boolean breaksAtDirection(Side aDirection) {
-		if(aDirection == Side.TOP) {
-			return breakableNorth;
-		} else if(aDirection == Side.BOTTOM) {
-			return breakableSouth;
-		} else if(aDirection == Side.RIGHT) {
-			return breakableEast;
-		} else if(aDirection == Side.LEFT) {
-			return breakableWest;
-		} 
-		return false;
+	public void execute(Map<Sprite, Side> myCollisionMap) {
+		//TODO NEED TO KNOW WHETHER ITS HORIZONTAL OR VERTICAL SCOLLER THIS IS SHITTY WAY OF TELLING MAX SCORE.
+		Action myAction = new ScoreAdder((int) Math.max(Math.abs(mySprite.getMyLocation().getXLocation()-initX), Math.abs(mySprite.getMyLocation().getYLocation()-initY)), mySprite);
+		myAction.act();
 	}
 
 	@Override
 	public Characteristic copy() {
-		return new ScoreBasedOnPosition(breakableNorth, breakableSouth, breakableEast, breakableWest, myDurability, mySprite);
+		return new ScoreBasedOnPosition(mySprite, initX, initY);
 	}
-
 
 }
