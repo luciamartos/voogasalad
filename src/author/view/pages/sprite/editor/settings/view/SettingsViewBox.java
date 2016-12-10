cbbe21ca80ec2daffab9338c084e32f0bd2f7de2
George Bernard <george.bernard@duke.edu>
2016-12-10 14:09:31 -0500

    extracted infomap class from viewbox
    Former-commit-id: a02cdc7c70556be91a8bf71257f042948f50d5c8

--- src/author/view/pages/sprite/editor/settings/view/SettingsViewBox.java
+++ src/author/view/pages/sprite/editor/settings/view/SettingsViewBox.java
@@ -17,43 +17,18 @@ import javafx.scene.layout.VBox;
 
 public class SettingsViewBox {
 
-	Pane myPane;
-	Map<String, Object> myInfo;
+	private Pane myPane;
+	private Map<String, Object> myInfo;
 
 	public SettingsViewBox(Object input) {
 		myInfo = new TreeMap<>();
 		myPane = new VBox();
 
-		buildInfoMap(input);
 		buildViewBox(input, myInfo);
 	}
 
 	public Pane getPane(){
 		return myPane;
-	}
-	
-	private void buildInfoMap(Object input) {
-		Method[] methods = input.getClass().getMethods();
-
-		for(Method m : methods){
-			List<Annotation> annotationList = Arrays.asList(m.getAnnotations());
-
-			ViewableMethodOutput vvAnnotation = null;
-
-			for(Annotation a : annotationList){
-				if(a instanceof ViewableMethodOutput){ 
-					ViewableMethodOutput vv = (ViewableMethodOutput) a;
-					try {
-						myInfo.put( vv.description(), m.invoke(input, (Object[]) null) );
-					} catch (ExceptionInInitializerError | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-						e.printStackTrace();
-						continue;
-					}
-				}
-			}
-
-			if(vvAnnotation == null) continue; 
-		}		
 	}
 
 	private void buildViewBox(Object input, Map<String, Object> infoMap){
