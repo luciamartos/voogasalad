bbcf9d212202cfca33174521a39ad660e4ac29dd
Hannah Fuchshuber <hpf3@duke.edu>
2016-12-10 15:59:48 -0500

    whoops
    Former-commit-id: 6ab7cd04730d28dfc9616878c902bf1dbbe4d90f

--- src/gameplayer/front_end/application_scene/INavigationDisplay.java
+++ src/gameplayer/front_end/application_scene/INavigationDisplay.java
@@ -1,11 +1,8 @@
 package gameplayer.front_end.application_scene;
 
-import gameplayer.front_end.gui_generator.IGUIGenerator.ButtonDisplay;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Node;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 
 public interface INavigationDisplay extends IDisplay {
 	
