9bfa6b3cac85d5b213803abf5595ed08e7ac30fd
Lucia Martos <lm260@duke.edu>
2016-12-11 15:34:58 -0500

    bugs
    Former-commit-id: fe8439865dc38f7c15cdb3bf5f2b20c43cc280b9

--- src/game_engine/actions/ScoreAdder.java
+++ src/game_engine/actions/ScoreAdder.java
@@ -1,6 +1,7 @@
 package game_engine.actions;
 
 import game_data.Sprite;
+import game_data.sprites.Player;
 import game_data.states.*;
 
 
@@ -14,8 +15,14 @@ public class ScoreAdder extends PointsChanger implements Action {
 
 	@Override
 	public void act() {
+//		System.out.println("in action");
+//		if(spriteAffected instanceof Player) System.out.println("is a player");
+
 		for(State i : spriteAffected.getStates()){
+
 			if (i instanceof Score){
+				System.out.println("has score");
+
 				//System.out.println(pointsToGive);
 				((Score) i).updateState(pointsToGive);
 //				if(((Health) i).getMyHealth()<=0){
